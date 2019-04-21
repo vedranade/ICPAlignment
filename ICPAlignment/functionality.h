1aea231a0ea25d743f750912a1b201ddf8afbd23
Ved Ranade <ved.ranade@gmail.com>
2019-04-21 17:54:49 -0400

    Add project files.

---
+++ ICPAlignment/functionality.h
@@ -0,0 +1,19 @@
+#include <glm/glm.hpp>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec2 texture_coord;
+	glm::vec3 normal;
+};
+
+struct VertRef
+{
+	VertRef(int v, int vt, int vn) : v(v), vt(vt), vn(vn) { }
+	int v, vt, vn;
+};
+
+std::vector<Vertex> loadOBJ(std::istream&);
---
+++ ICPAlignment/main.cpp
@@ -0,0 +1,168 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/component_wise.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "functionality.h"
+
+int btn;
+glm::ivec2 startMouse;
+glm::ivec2 startRot, curRot;
+glm::ivec2 startTrans, curTrans;
+
+std::vector<Vertex> model;
+
+template< typename Vec >
+std::pair< Vec, Vec > GetExtents(const Vec* pts, size_t stride, size_t count)
+{
+	unsigned char* base = (unsigned char*)pts;
+	Vec pmin(*(Vec*)base);
+	Vec pmax(*(Vec*)base);
+	for (size_t i = 0; i < count; ++i, base += stride)
+	{
+		const Vec& pt = *(Vec*)base;
+		pmin = glm::min(pmin, pt);
+		pmax = glm::max(pmax, pt);
+		
+	}
+
+	return std::make_pair(pmin, pmax);
+}
+
+template<typename Vec>
+void CenterAndScale(Vec *pts, size_t stride, size_t count, const typename Vec::value_type & size)
+{
+	typedef typename Vec::value_type Scalar;
+
+	// get min/max extents
+	std::pair< Vec, Vec > exts = GetExtents(pts, stride, count);
+
+	// center and scale 
+	const Vec center = (exts.first * Scalar(0.5)) + (exts.second * Scalar(0.5f));
+
+	const Scalar factor = size / glm::compMax(exts.second - exts.first);
+	unsigned char* base = (unsigned char*)pts;
+	for (size_t i = 0; i < count; ++i, base += stride)
+	{
+		Vec& pt = *(Vec*)base;
+		pt = ((pt - center) * factor);
+	}
+	
+}
+
+void display()
+{
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double w = glutGet(GLUT_WINDOW_WIDTH);
+	double h = glutGet(GLUT_WINDOW_HEIGHT);
+	double ar = w / h;
+	glTranslatef(curTrans.x / w * 2, curTrans.y / h * 2, 0);
+	gluPerspective(60, ar, 0.1, 100);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0, 0, -10);
+
+	glPushMatrix();
+	{
+		glRotatef(curRot.x % 360, 0, 1, 0);
+		glRotatef(-curRot.y % 360, 1, 0, 0);
+
+		// object
+		glColor3ub(255, 0, 0);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &model[0].position);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &model[0].texture_coord);
+		glNormalPointer(GL_FLOAT, sizeof(Vertex), &model[0].normal);
+		glDrawArrays(GL_TRIANGLES, 0, model.size());
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisableClientState(GL_NORMAL_ARRAY);
+
+		//// bounding cube
+		//glDisable(GL_LIGHTING);
+		//glColor3ub(255, 255, 255);
+		//glutWireCube(7);
+		//glEnable(GL_LIGHTING);
+	}
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		btn = button;
+		startMouse = glm::ivec2(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+		startRot = curRot;
+	}
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		btn = button;
+		startMouse = glm::ivec2(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+		startTrans = curTrans;
+	}
+}
+
+void motion(int x, int y)
+{
+	glm::ivec2 curMouse(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+	if (btn == GLUT_RIGHT_BUTTON)
+	{
+		curRot = startRot + (curMouse - startMouse);
+	}
+	else if (btn == GLUT_LEFT_BUTTON)
+	{
+		curTrans = startTrans + (curMouse - startMouse);
+	}
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	std::ifstream inputfile("lamp.obj");
+	model = loadOBJ(inputfile);
+	CenterAndScale(&model[0].position, sizeof(Vertex), model.size(), 7);
+	
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutInitWindowSize(1280, 720);
+	glutCreateWindow("OBJ");
+	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+
+	glEnable(GL_DEPTH_TEST);
+
+	// set up "headlamp"-like light
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	GLfloat position[] = { 0, 0, 1, 0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glPolygonMode(GL_BACK, GL_LINE);
+	glutMainLoop();
+
+	return 0;
+}
