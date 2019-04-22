245c180578815fb2d06f9a6b11a171bfee21afa7
Ved Ranade <ved.ranade@gmail.com>
2019-04-22 15:15:38 -0400

    Created display window for second object.

--- ICPAlignment/main.cpp
+++ ICPAlignment/main.cpp
@@ -17,7 +17,8 @@ glm::ivec2 startRot, curRot;
 glm::ivec2 startTrans, curTrans;
 GLfloat zoom = 0.0f;
 
-std::vector<Vertex> model;
+std::vector<Vertex> first_model;
+std::vector<Vertex> second_model;
 
 template< typename Vec >
 std::pair< Vec, Vec > GetExtents(const Vec* pts, size_t stride, size_t count)
@@ -57,7 +58,7 @@ void CenterAndScale(Vec *pts, size_t stride, size_t count, const typename Vec::v
 	
 }
 
-void display()
+void display_first()
 {
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -84,10 +85,56 @@ void display()
 		glEnableClientState(GL_VERTEX_ARRAY);
 		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		glEnableClientState(GL_NORMAL_ARRAY);
-		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &model[0].position);
-		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &model[0].texture_coord);
-		glNormalPointer(GL_FLOAT, sizeof(Vertex), &model[0].normal);
-		glDrawArrays(GL_TRIANGLES, 0, model.size());
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &first_model[0].position);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &first_model[0].texture_coord);
+		glNormalPointer(GL_FLOAT, sizeof(Vertex), &first_model[0].normal);
+		glDrawArrays(GL_TRIANGLES, 0, first_model.size());
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisableClientState(GL_NORMAL_ARRAY);
+
+		// bounding cube
+		glDisable(GL_LIGHTING);
+		/*glColor3ub(255, 255, 255);
+		glutWireCube(7);*/
+		glEnable(GL_LIGHTING);
+	}
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void display_second()
+{
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double w = glutGet(GLUT_WINDOW_WIDTH);
+	double h = glutGet(GLUT_WINDOW_HEIGHT);
+	double ar = w / h;
+	glTranslatef(curTrans.x / w * 2, curTrans.y / h * 2, zoom);
+	gluPerspective(60, ar, 0.1, 100);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0, 0, -10);
+
+	glPushMatrix();
+	{
+		glRotatef(curRot.x % 360, 0, 1, 0);
+		glRotatef(-curRot.y % 360, 1, 0, 0);
+
+		// object
+		glColor3ub(255, 0, 0);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &second_model[0].position);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &second_model[0].texture_coord);
+		glNormalPointer(GL_FLOAT, sizeof(Vertex), &second_model[0].normal);
+		glDrawArrays(GL_TRIANGLES, 0, second_model.size());
 		glDisableClientState(GL_VERTEX_ARRAY);
 		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 		glDisableClientState(GL_NORMAL_ARRAY);
@@ -143,16 +190,40 @@ void motion(int x, int y)
 
 int main(int argc, char **argv)
 {
-	std::ifstream inputfile("lamp.obj");
-	model = loadOBJ(inputfile);
-	CenterAndScale(&model[0].position, sizeof(Vertex), model.size(), 7);
+	std::ifstream inputfile1("lamp.obj");
+	first_model = loadOBJ(inputfile1);
+	CenterAndScale(&first_model[0].position, sizeof(Vertex), first_model.size(), 7);
+
+	std::ifstream inputfile2("dodecahedron.obj");
+	second_model = loadOBJ(inputfile2);
+	CenterAndScale(&second_model[0].position, sizeof(Vertex), second_model.size(), 7);
 	
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
 	glutInitWindowSize(1280, 720);
-	glutCreateWindow("OBJ");
-	glutDisplayFunc(display);
+	glutCreateWindow("First object");
+	glutDisplayFunc(display_first);
+	glutMouseFunc(mouse);
+	glutMotionFunc(motion);
+	//glutMouseWheelFunc(mouse_wheel);
+
+	// set up simple lighting:
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	GLfloat position[] = { 0, 0, 1, 0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glPolygonMode(GL_BACK, GL_LINE);
+
+	glutCreateWindow("Second object");
+	glutDisplayFunc(display_second);
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
 	//glutMouseWheelFunc(mouse_wheel);
@@ -167,7 +238,7 @@ int main(int argc, char **argv)
 	glEnable(GL_LIGHT0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	GLfloat position[] = { 0, 0, 1, 0 };
+	//GLfloat position[] = { 0, 0, 1, 0 };
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
 	glPolygonMode(GL_FRONT, GL_FILL);
